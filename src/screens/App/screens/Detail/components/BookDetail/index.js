e0bb29d8928f0b90adc8b85b84de68e12bf5a705
Melisa Rossi <melisa.rossi@wolox.com.ar>
2017-09-07 16:22:14 -0300

    fix style isuues book detail

--- src/screens/App/screens/Detail/components/BookDetail/index.js
+++ src/screens/App/screens/Detail/components/BookDetail/index.js
@@ -2,19 +2,20 @@ import React from 'react';
 
 import './style.css'
 import defaultBook from '../../../../../../assets/Book/default_book.svg'
+import {RENT} from './strings'
 
-const BookDetail = (props) => {
+const BookDetail = ({ book }) => {
   return (
     <div className='detail-book'>
       <div className='detail-book-image-box'>
-        <img src={props.book.image_url || defaultBook} alt={props.book.title} className='detail-book-image' />
+        <img src={book.image_url || defaultBook} alt={book.title} className='detail-book-image' />
       </div>
       <div className='detail-information'>
-        <p className='detail-title'>{props.book.title}</p>
-        <p className='detail-author detail-text'>{props.book.author}</p>
-        <p className='detail-text'>{props.book.year}</p>
-        <p className='detail-text'>{props.book.genre}</p>
-        <button type='button' className='rent-button'>Alquilar</button>
+        <p className='detail-title'>{book.title}</p>
+        <p className='detail-author detail-text'>{book.author}</p>
+        <p className='detail-text'>{book.year}</p>
+        <p className='detail-text'>{book.genre}</p>
+        <button type='button' className='rent-button'>{RENT}</button>
       </div>
     </div>
   )
