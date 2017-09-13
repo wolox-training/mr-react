8a9fceaef90702dcde050726c8a0ebc9617e1b5e
Melisa Rossi <melisa.rossi@wolox.com.ar>
2017-09-13 12:07:46 -0300

    fix style issues

--- src/screens/App/components/Navbar/components/UserPopover/index.js
+++ src/screens/App/components/Navbar/components/UserPopover/index.js
@@ -5,13 +5,13 @@ import UserPopover from './layout';
 
 class UserPopoverContainer extends Component {
 
-  logout = () => {
+  handleLogout = () => {
     localStorage.removeItem('access_token');
     this.props.history.push('/login');
   }
 
   render() {
-      return <UserPopover onLogout={this.logout}/>
+      return <UserPopover onLogout={this.handleLogout}/>
   }
 }
 
